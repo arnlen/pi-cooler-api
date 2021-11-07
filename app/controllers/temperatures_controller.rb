0694248c43f0d8c4927a303ae2761e56008bc8d9
Arnaud Lenglet <arlenglet@gmail.com>
2021-11-07 19:25:11 +0100

    feat(Temperatures): implement #create in html + json

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,5 +1,5 @@
 class TemperaturesController < ApplicationController
-  before_action :set_temperature, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /temperatures or /temperatures.json
   def index
@@ -15,53 +15,18 @@ class TemperaturesController < ApplicationController
     @temperature = Temperature.new
   end
 
-  # GET /temperatures/1/edit
-  def edit
-  end
-
   # POST /temperatures or /temperatures.json
   def create
     @temperature = Temperature.new(temperature_params)
+    Temperature.add(pi_name: @temperature.pi_name, reading: @temperature.reading)
 
     respond_to do |format|
-      if @temperature.save
-        format.html { redirect_to @temperature, notice: "Temperature was successfully created." }
-        format.json { render :show, status: :created, location: @temperature }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @temperature.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /temperatures/1 or /temperatures/1.json
-  def update
-    respond_to do |format|
-      if @temperature.update(temperature_params)
-        format.html { redirect_to @temperature, notice: "Temperature was successfully updated." }
-        format.json { render :show, status: :ok, location: @temperature }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @temperature.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /temperatures/1 or /temperatures/1.json
-  def destroy
-    @temperature.destroy
-    respond_to do |format|
-      format.html { redirect_to temperatures_url, notice: "Temperature was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to temperatures_path, notice: "Temperature was successfully created." }
+      format.json { head :created }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_temperature
-      @temperature = Temperature.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def temperature_params
       params.require(:temperature).permit(:pi_name, :reading)
