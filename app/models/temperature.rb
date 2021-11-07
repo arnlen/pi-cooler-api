a25d19a3396871efdcd81fa5e75a2ccb9d8f516c
Arnaud Lenglet <arlenglet@gmail.com>
2021-11-07 18:55:00 +0100

    feat(Temperatures): create $temperatures hash + disable logger for development env

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -1,2 +1,20 @@
 class Temperature < ApplicationRecord
+
+  $temperatures = {
+    cooler: [],
+    router: [],
+    compute_1: [],
+    compute_2: [],
+    compute_3: [],
+    compute_4: [],
+  }
+
+  def self.all
+    $temperatures
+  end
+
+  def self.add(pi_name:, reading:)
+    $temperatures[pi_name.to_sym].push(reading)
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Arnlen: disable logging to preserve microSD cards on Raspberry
+  config.logger = Logger.new('/dev/null')
 end
