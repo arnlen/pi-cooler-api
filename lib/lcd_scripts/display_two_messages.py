1fb0eaa9ac6568180773db7411c9d8d5a06fb8cb
Arnaud Lenglet <arlenglet@gmail.com>
2021-12-27 17:10:30 +0100

    feat(lcd): finish first working of two messages script

--- lib/lcd_scripts/display_two_messages.py
+++ lib/lcd_scripts/display_two_messages.py
@@ -6,8 +6,14 @@ import sys
 
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 
-message1 = sys.argv[1]
-message2 = sys.argv[2]
+message1 = ""
+message2 = ""
+
+if len(sys.argv) > 1:
+    message1 = sys.argv[1]
+
+if len(sys.argv) > 2:
+    message2 = sys.argv[2]
 
 def getIp(interface):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,10 +27,10 @@ if message1 == "ip":
     message1 = "Alpha IP:"
     message2 = getIp('wlan0') # For Ethernet, switch to: getIp('eth0')
 
-print("message 1: \"%s\"", message1)
-print("message 2: \"%s\"", message2)
+print('message 1:', message1)
+print("message 2:", message2)
 
 lcd.cursor_pos = (0, 0)
-lcd.write_string("%s", message1)
+lcd.write_string(message1)
 lcd.cursor_pos = (1, 0)
-lcd.write_string("%s", message2)
+lcd.write_string(message2)
