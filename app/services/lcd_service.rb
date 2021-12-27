3f27d28258969eafb1458094fe7437e58cc0b84d
Arnaud Lenglet <arlenglet@gmail.com>
2021-12-27 18:19:32 +0100

    fix(lcd): fix LcdService to use python3 + add debug output

--- app/services/lcd_service.rb
+++ app/services/lcd_service.rb
@@ -3,9 +3,10 @@ class LcdService
   @@display_list = []
 
   def self.display(message)
-    @@display_list.push(message)
+    @@display_list.push({ message: message })
     @@display_list = @@display_list.drop(1) if @@display_list.size > 2
-    `python lib/lcd_scripts/display_two_messages.py #{@@display_list[0]} #{@@display_list[1]}`
+    puts @@display_list
+    `python3 lib/lcd_scripts/display_two_messages.py #{@@display_list[0][:message]} #{@@display_list[1][:message] if @@display_list[1]}`
   end
 
 end
