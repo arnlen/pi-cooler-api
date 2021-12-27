17f4dbd546f23fcf1fbab065e2d032b185974402
Arnaud Lenglet <arlenglet@gmail.com>
2021-12-27 19:19:25 +0100

    fix(lcd): convert readings from float to int to reduce size on display

--- app/services/lcd_service.rb
+++ app/services/lcd_service.rb
@@ -5,7 +5,7 @@ class LcdService
   def self.display(pi_name, temperature_reading)
     self.add_to_list(pi_name, temperature_reading)
     puts @@dlist
-    argv = "\"#{@@dlist[0][:pi_short_name]} #{@@dlist[0][:temperature]}    #{@@dlist[1][:pi_short_name] if @@dlist[1]} #{@@dlist[1][:temperature] if @@dlist[1]}\" \"#{@@dlist[2][:pi_short_name] if @@dlist[2]} #{@@dlist[2][:temperature] if @@dlist[2]}    #{@@dlist[3][:pi_short_name] if @@dlist[3]} #{@@dlist[3][:temperature] if @@dlist[3]}\""
+    argv = "\"#{@@dlist[0][:pi_short_name]} #{@@dlist[0][:temperature]}  #{@@dlist[1][:pi_short_name] if @@dlist[1]} #{@@dlist[1][:temperature] if @@dlist[1]}\" \"#{@@dlist[2][:pi_short_name] if @@dlist[2]} #{@@dlist[2][:temperature] if @@dlist[2]}  #{@@dlist[3][:pi_short_name] if @@dlist[3]} #{@@dlist[3][:temperature] if @@dlist[3]}\""
     puts "argv: #{argv}"
     `python3 lib/lcd_scripts/display_two_messages.py #{argv}`
   end
@@ -16,8 +16,8 @@ class LcdService
     else
       @@dlist.push({
         pi_name: pi_name,
-        pi_short_name: pi_name[0..2],
-        temperature: temperature_reading
+        pi_short_name: pi_name[0..3].capitalize,
+        temperature: temperature_reading.to_i
       })
     end
   end
