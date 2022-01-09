db3b031930b9800379f8991d5cdb2d17f7c45a01
Arnaud Lenglet <arlenglet@gmail.com>
2022-01-09 22:53:01 +0100

    refactor(LcdService): reduce complexity of refresh_readings method

--- app/services/lcd_service.rb
+++ app/services/lcd_service.rb
@@ -1,46 +1,51 @@
 class LcdService
 
-  @@last_display_updated_at = DateTime.now
-
-  def self.last_display_updated_at
-    @@last_display_updated_at
-  end
+  @@display_last_updated_at = DateTime.now
 
   def self.should_update_display?
-    @@last_display_updated_at < 30.seconds.ago
+    puts "Display last updated at: #{@@display_last_updated_at}"
+    @@display_last_updated_at < 30.seconds.ago
   end
 
   def self.refresh_readings
     puts "Refreshing display..."
 
-    readings = Temperature.last_temperatures_per_pi.collect do |last_temperature|
-      {
-        pi_short_name: last_temperature.pi_short_name,
-        reading: last_temperature.reading
-      }
-    end
-
+    readings = LcdService.get_readings
     puts "Readings: #{readings}"
 
-    positions = readings.each_with_index.collect do |reading, index|
-      "#{readings[index][:pi_short_name]} #{readings[index][:reading]}"
-    end
-
+    positions = LcdService.format_readings_to_positions(readings)
     puts "Positions: #{positions}"
 
     first_line = "#{positions[0]} #{positions[1]}"
     second_line = "#{positions[2]} #{positions[3]}"
-
     puts "First line: #{first_line}"
     puts "Second line: #{second_line}"
 
     argv = "\"#{first_line}\" \"#{second_line}\""
-
     puts "Screen will be refresh to: #{argv}"
 
-    `python3 lib/lcd_scripts/display_two_messages.py #{argv}`
+    LcdService.display_two_messages_to_screen(argv)
 
-    @@last_display_updated_at = DateTime.now
+    @@display_last_updated_at = DateTime.now
+  end
+
+  def self.display_two_messages_to_screen(argv)
+    `python3 lib/lcd_scripts/display_two_messages.py #{argv}`
+  end
+
+  def self.get_readings
+    Temperature.last_temperatures_per_pi.collect do |last_temperature|
+      {
+        pi_short_name: last_temperature.pi_short_name,
+        reading: last_temperature.reading.round
+      }
+    end
+  end
+
+  def self.format_readings_to_positions(readings)
+    readings.each_with_index.collect do |reading, index|
+      "#{readings[index][:pi_short_name]} #{readings[index][:reading]}"
+    end
   end
 
 end
